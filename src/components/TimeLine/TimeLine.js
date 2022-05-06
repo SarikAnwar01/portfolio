d67fd8939570ca11deeb1f49f79b1ccd7832508f
Sarik Anwar <sarikanwar007@gmail.com>
2022-05-06 14:32:58 +0545

    temp

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -46,7 +46,7 @@ const Timeline = () => {
     <Section id="about">
       <SectionTitle>About Me</SectionTitle>
       <SectionText>
-        The purpose of JavaScript Mastery is to help aspiring and established developers to take their development skills to the next level and build awesome apps.
+        I am a full stack web developer with a background in graphic design and a passion for creating beautiful and intuitive user experiences.
       </SectionText>
       <CarouselContainer ref={carouselRef} onScroll={handleScroll}>
         <>
